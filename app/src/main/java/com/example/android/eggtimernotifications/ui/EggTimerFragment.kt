c6f28d8f13ea93c896c3ad1628052688c18af3ad
kitsu <kitsu@mail.com>
2020-10-13 18:01:22 +0200

    1.3 subscribing to a topic

--- app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
@@ -61,7 +61,8 @@ class EggTimerFragment : Fragment() {
             getString(R.string.breakfast_notification_channel_id),
             getString(R.string.breakfast_notification_channel_name)
         )
-        // TODO: Step 3.4 call subscribe topics on start
+        // Step 3.4 call subscribe topics on start
+        subscribeTopic()
 
         return binding.root
     }
@@ -95,7 +96,18 @@ class EggTimerFragment : Fragment() {
         // TODO: Step 1.6 END create channel
     }
 
-    // TODO: Step 3.3 subscribe to breakfast topic
+    // Step 3.3 subscribe to breakfast topic
+    private fun subscribeTopic(){
+         FirebaseMessaging.getInstance().subscribeToTopic(TOPIC)
+             .addOnCompleteListener { task ->
+                 var msg = getString(R.string.message_subscribed)
+                 if (!task.isSuccessful) {
+                     msg = getString(R.string.message_subscribe_failed)
+                 }
+                 Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+             }
+
+    }
 
     companion object {
         fun newInstance() = EggTimerFragment()
